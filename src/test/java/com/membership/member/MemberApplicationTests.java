e29d63e186687624200199dd775eefb62138d0a2
wonnho71 <wonnho@nate.com>
2026-09-09 22:41:36 +0900

    first git, adios amigo. use @ResponseBody or view template

---
+++ src/test/java/com/membership/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.membership.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
