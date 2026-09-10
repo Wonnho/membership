4ee719767cfaad73458be998a32ca03e47f1e316
wonnho71 <wonnho@nate.com>
2026-09-10 15:05:57 +0900

    create a from of membership join

--- src/main/java/com/membership/member/controller/MemberController.java
+++ src/main/java/com/membership/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.membership.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,5 +14,12 @@ public class MemberController {
     public String adios(Model model) {
         model.addAttribute("amigos","Amigos");
         return "front";
+    }
+
+    @PostMapping("/join")
+    public String join() {
+
+        return "/newmember";
+
     }
 }
