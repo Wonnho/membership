df9d510641241f1f425c9664ad75ecdd4b24768b
wonnho71 <wonnho@nate.com>
2026-09-10 14:27:25 +0900

    layouts ->header.mustache using Navbar of getbootstrap. footer.mustache. insert two header and footer into front.mustache {{>layouts/header}}

--- src/main/java/com/membership/member/controller/MemberController.java
+++ src/main/java/com/membership/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.membership.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MemberController {
 
     @GetMapping("/adios")
-    @ResponseBody
-    public String adio() {
-        return "header?";
+    //@ResponseBody
+    public String adios(Model model) {
+        model.addAttribute("amigos","Amigos");
+        return "front";
     }
 }
